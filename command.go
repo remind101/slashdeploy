9fedfb5696d3c1a06d4a5bcd515a76a344e0f2d3
Eric J. Holmes <eric@ejholmes.net>
2015-12-05 12:17:27 +0700

    Add example.

--- command.go
+++ command.go
@@ -81,4 +81,5 @@ func splitRepo(fullName string) (repo, owner string, err error) {
 }
 
 var helpText = `To deploy a repo to the default environment: /deploy REPO
-To deploy a repo to a specific environment: /deploy REPO to ENVIRONMENT`
+To deploy a repo to a specific environment: /deploy REPO to ENVIRONMENT
+To deploy a repo to a specific branch: /deploy REPO@REF to ENVIRONMENT`
