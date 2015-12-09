1398d389351a16dc9e2762db1604a3201f1efbb7
Eric J. Holmes <eric@ejholmes.net>
2015-12-09 16:58:56 +0700

    Cleanup.

--- slashdeploy.go
+++ slashdeploy.go
@@ -3,20 +3,7 @@ package slashdeploy
 import (
 	"github.com/ejholmes/slashdeploy/deployments"
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
 )
-
-// Defaults unless overriden by the repository settings.
-var (
-	DefaultEnvironment = "production"
-	DefaultRef         = "master"
-)
-
-func init() {
-	// We cannot set the client_secret in the auth header, which is the
-	// default behavior of package oauth2.
-	oauth2.RegisterBrokenAuthHeaderProvider("https://slack.com/api")
-}
 
 type SlashDeploy struct {
 	Users UsersStore
