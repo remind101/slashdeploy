775b520c4623e3ff317bdf6c0b16e7129ffd86b1
Eric J. Holmes <eric@ejholmes.net>
2016-02-03 19:32:16 +0700

    Update help.

--- app/commands/help_command.rb
+++ app/commands/help_command.rb
@@ -5,6 +5,7 @@ class HelpCommand < BaseCommand
 To deploy a repo to the default environment: /deploy REPO
 To deploy a repo to a specific environment: /deploy REPO to ENVIRONMENT
 To deploy a repo to a specific branch: /deploy REPO@REF to ENVIRONMENT
+To force a deployment, ignoring any commit statuses: /deploy REPO!
 To list known environments you can deploy a repo to: /deploy where REPO
 To lock an environment: /deploy lock ENVIRONMENT on REPO: MESSAGE
 To unlock a previously locked environment: /deploy unlock ENVIRONMENT on REPO
