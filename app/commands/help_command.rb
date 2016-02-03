002010e6f7fbb26bcd5c247c74bab1668937ff98
Eric J. Holmes <eric@ejholmes.net>
2016-02-03 19:31:21 +0700

    Update usage.

--- app/commands/help_command.rb
+++ app/commands/help_command.rb
@@ -6,7 +6,8 @@ To deploy a repo to the default environment: /deploy REPO
 To deploy a repo to a specific environment: /deploy REPO to ENVIRONMENT
 To deploy a repo to a specific branch: /deploy REPO@REF to ENVIRONMENT
 To list known environments you can deploy a repo to: /deploy where REPO
-To lock an environment on a repo: /deploy lock ENVIRONMENT on REPO: MESSAGE
+To lock an environment: /deploy lock ENVIRONMENT on REPO: MESSAGE
+To unlock a previously locked environment: /deploy unlock ENVIRONMENT on REPO
 EOF
 
   def run(_user, _cmd, _params)
