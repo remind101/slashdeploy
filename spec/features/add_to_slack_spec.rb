ed4f249cc0a988a85048a8145580966f715be6b5
Eric J. Holmes <eric@ejholmes.net>
2016-02-06 19:43:45 +0700

    Rubocop.

--- spec/features/add_to_slack_spec.rb
+++ spec/features/add_to_slack_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Add to Slack' do
 
   scenario 'clicking the Add to Slack button' do
     stub_request(:post, 'https://slack.com/api/oauth.access')
-      .with(body: { 'client_id' => '', 'client_secret' => '', 'code' => 'code', 'grant_type' => 'authorization_code'})
+      .with(body: { 'client_id' => '', 'client_secret' => '', 'code' => 'code', 'grant_type' => 'authorization_code' })
       .to_return(status: 200, body: { 'access_token' => 'xoxt-23984754863-2348975623103', 'scope' => 'read' }.to_json, headers: { 'Content-Type': 'application/json' })
     visit '/auth/slack/callback?code=code'
     expect(page).to have_content 'Success!'
