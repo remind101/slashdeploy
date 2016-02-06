03c1a3a35f28a9329d8c90dd72b2f82019a95b86
Eric J. Holmes <eric@ejholmes.net>
2016-02-07 00:01:50 +0700

    Serve static assets.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module SlashDeploy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # https://devcenter.heroku.com/articles/rails-4-asset-pipeline#serve-assets
+    config.serve_static_assets = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
