c248082534a4cbb35d362400297637333098ef1e
Eric J. Holmes <eric@ejholmes.net>
2016-02-06 21:37:37 +0700

    List deployments to determine access.

--- lib/slashdeploy/authorizer/github.rb
+++ lib/slashdeploy/authorizer/github.rb
@@ -2,9 +2,18 @@ module SlashDeploy
   module Authorizer
     # An authorizer that uses the github api to check if the user has
     # permissions to access the repository.
+    #
+    # Ideally, we'd use the collaborators API (https://goo.gl/B48TiU).
+    # Unfortunately, that requires obtaining the `repo` scope. Instead, this
+    # implementation attempts to read the deployments on the repo. If that
+    # succeeds, we know they have access to deploy.
     class GitHub
       def authorized?(user, repository)
-        user.github_client.collaborator?(repository, user.github_login)
+        # Add a fake sha so we don't get any response data.
+        user.github_client.deployments(repository, sha: '1')
+        true
+      rescue Octokit::NotFound
+        false
       end
     end
   end
--- spec/slashdeploy/authorizer/github_spec.rb
+++ spec/slashdeploy/authorizer/github_spec.rb
@@ -6,16 +6,18 @@ RSpec.describe SlashDeploy::Authorizer::GitHub do
   let(:authorizer) { described_class.new }
 
   describe '#authorized?' do
-    context 'when the user is a collaborator on the repo' do
+    context 'when the user has access to the deployments of the repo' do
       it 'returns true' do
-        expect(client).to receive(:collaborator?).and_return(true)
+        expect(client).to receive(:deployments).with('remind101/acme-inc', sha: '1')
         expect(authorizer.authorized? user, 'remind101/acme-inc').to be_truthy
       end
     end
 
-    context 'when the user is not a collaborator on the repo' do
+    context 'when the user does not have access to the deployments of the repo' do
       it 'returns false' do
-        expect(client).to receive(:collaborator?).and_return(false)
+        expect(client).to receive(:deployments).with('remind101/acme-inc', sha: '1').and_raise(
+          Octokit::NotFound.new
+        )
         expect(authorizer.authorized? user, 'remind101/acme-inc').to be_falsey
       end
     end
