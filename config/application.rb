67a2bebf3d741efe1037880913e26495ff9e60e0
Eric J. Holmes <eric@ejholmes.net>
2016-02-07 08:35:56 +0700

    Precompile fonts.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module SlashDeploy
     config.serve_static_files = true
     config.assets.version = '2'
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
