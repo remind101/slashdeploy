4e054b2d42d91451ecd8e1c5dc8d6bd1dc2ef093
Eric Holmes <eric@ejholmes.io>
2016-02-08 13:55:50 +0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 require 'webmock/rspec'
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
 require 'slash'
 require 'slashdeploy'
 
