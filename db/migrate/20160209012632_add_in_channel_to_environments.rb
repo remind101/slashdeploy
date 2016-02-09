cc4234ea7e6b6fec27715363484587bd7061da40
Eric J. Holmes <eric@ejholmes.net>
2016-02-09 10:57:43 +0700

    Ability to rollback migration.

--- db/migrate/20160209012632_add_in_channel_to_environments.rb
+++ db/migrate/20160209012632_add_in_channel_to_environments.rb
@@ -1,8 +1,12 @@
 class AddInChannelToEnvironments < ActiveRecord::Migration
-  def change
+  def up
     add_column :environments, :in_channel, :boolean, null: false, default: false
     execute <<-SQL
     UPDATE environments SET in_channel = 't' WHERE name = 'production'
     SQL
+  end
+  
+  def down
+    remove_column :environments, :in_channel
   end
 end
