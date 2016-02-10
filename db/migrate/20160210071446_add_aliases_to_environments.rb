917f0b04b74e582938d2e96f03405f43167448c0
Eric J. Holmes <eric@ejholmes.net>
2016-02-10 16:36:31 +0700

    Add aliases.

--- db/migrate/20160210071446_add_aliases_to_environments.rb
+++ db/migrate/20160210071446_add_aliases_to_environments.rb
@@ -2,5 +2,15 @@ class AddAliasesToEnvironments < ActiveRecord::Migration
   def change
     add_column :environments, :aliases, :text, array: true, default: []
     add_index :environments, [:repository_id, :name], unique: true
+    execute <<-SQL
+    UPDATE environments SET aliases = (CASE name
+      WHEN 'production' THEN '{"prod"}'::text[]
+      WHEN 'staging' THEN '{"stage"}'::text[]
+      END);
+    SQL
+  end
+
+  def down
+    drop_column :environments, :aliases
   end
 end
