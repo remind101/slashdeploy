13d6b593807bb87d4238c64032679f87781ce130
Eric J. Holmes <eric@ejholmes.net>
2016-02-17 16:26:26 +0700

    Respond with a 204 for unhandled github events. Fixes #41.

--- lib/hookshot/router.rb
+++ lib/hookshot/router.rb
@@ -10,6 +10,9 @@ module Hookshot
   #   router.handle :push, PushHandler.new
   #   router.handle :deployment, DeploymentHandler.new
   class Router
+    # Rack app that gets called when a handler is not found.
+    attr_accessor :not_found
+
     def apps
       @apps ||= {}
     end
@@ -20,8 +23,12 @@ module Hookshot
 
     def call(env)
       app = apps[env[HEADER_GITHUB_EVENT].to_sym]
-      return [404, {}, ['']] unless app
+      return not_found.call(env) unless app
       app.call(env)
+    end
+
+    def not_found
+      @not_found ||= -> (_env) { [204, {}, ['']] }
     end
   end
 end
