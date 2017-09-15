c10921335a1a640d55dbfb96c5727686f3b1b87d
Eric J. Holmes <eric@ejholmes.net>
2017-09-14 20:50:34 -0700

    Converts DeploymentRequest to plain struct

--- app/models/deployment_request.rb
+++ app/models/deployment_request.rb
@@ -1,15 +1,10 @@
 # A DeploymentRequest represents the options provided when requesting a new deployment.
-class DeploymentRequest
-  include Virtus.value_object
-
-  values do
-    # The repository to deploy.
-    attribute :repository, String
-    # The git branch, tag or commit to deploy.
-    attribute :ref, String
-    # The environment to deploy to.
-    attribute :environment, String
-    # Whether to "force" the deployment or not (i.e. Ignore commit status contexts).
-    attribute :force, Boolean
+DeploymentRequest = Struct.new(:repository, :ref, :environment, :force) do
+  def initialize(opts = {})
+    super \
+      opts[:repository],
+      opts[:ref],
+      opts[:environment],
+      opts[:force]
   end
 end
