4369900a580affee38f5087a3e56fa6ca6867b9d
Russell Ballestrini <russell.ballestrini@gmail.com>
2018-12-17 16:51:57 -0800

    I think this will help end users fix themselves
    	modified:   app/models/slack_bot.rb

--- app/models/slack_bot.rb
+++ app/models/slack_bot.rb
@@ -3,9 +3,13 @@ class SlackBot < ActiveRecord::Base
 
   def self.from_auth_hash(auth_hash)
     bot_info = auth_hash[:extra][:bot_info]
-    find_or_create_by(id: bot_info[:bot_user_id]) do |bot|
+    bot = find_or_create_by(id: bot_info[:bot_user_id]) do |bot|
       bot.access_token = bot_info[:bot_access_token]
       bot.slack_team_id = auth_hash[:info][:team_id]
     end
+    if bot.access_token != bot_info[:bot_access_token]
+      bot.access_token = bot_info[:bot_access_token]
+    end
+    return bot
   end
 end
