cd2626ca2da782e76c40131fa3bff308713b2b8c
june-remind101 <june-remind101@users.noreply.github.com>
2020-07-09 13:16:19 -0700

    define the on/to regex once

--- app/commands/slash_commands.rb
+++ app/commands/slash_commands.rb
@@ -5,6 +5,7 @@ class SlashCommands
   REPO = /(?<repository>\S+?)/
   ENV  = /(?<environment>\S+?)/
   REF  = /(?<ref>\S+?)/
+  ON_TO = /(on|to){1}/
 
   attr_reader :router
 
@@ -13,13 +14,13 @@ class SlashCommands
     router = Slash::Router.new
     router.match match_regexp(/^help$/), HelpCommand
     router.match match_regexp(/^where #{REPO}$/), EnvironmentsCommand
-    router.match match_regexp(/^lock #{ENV} (on|to){1} #{REPO}(:(?<message>.*(?<!\!$)))?(?<force>!)?$/), LockCommand
+    router.match match_regexp(/^lock #{ENV} #{ON_TO} #{REPO}(:(?<message>.*(?<!\!$)))?(?<force>!)?$/), LockCommand
     router.match match_regexp(/^unlock all$/), UnlockAllCommand
-    router.match match_regexp(/^unlock #{ENV} (on|to){1} #{REPO}$/), UnlockCommand
-    router.match match_regexp(/^check #{ENV} (on|to){1} #{REPO}$/), CheckCommand
+    router.match match_regexp(/^unlock #{ENV} #{ON_TO} #{REPO}$/), UnlockCommand
+    router.match match_regexp(/^check #{ENV} #{ON_TO} #{REPO}$/), CheckCommand
     router.match match_regexp(/^boom$/), BoomCommand
-    router.match match_regexp(/^#{REPO}(@#{REF})?( (on|to){1} #{ENV})?(?<force>!)?$/), DeployCommand
-    router.match match_regexp(/^latest #{REPO}( (on|to){1} #{ENV})?$/), LatestCommand
+    router.match match_regexp(/^#{REPO}(@#{REF})?( #{ON_TO} #{ENV})?(?<force>!)?$/), DeployCommand
+    router.match match_regexp(/^latest #{REPO}( #{ON_TO} #{ENV})?$/), LatestCommand
 
     router.not_found = -> (env) do
       env['params'] = { 'not_found' => true }
