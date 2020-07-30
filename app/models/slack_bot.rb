73642328f8fe1faa3b4b9fdda07a76301d6148b4
Jack <46763492+aengelas@users.noreply.github.com>
2020-07-30 15:25:27 -0700

    Apply suggestions from code review
    Co-authored-by: Benjamin Guillet <benjamin.guillet@gmail.com>

--- app/models/slack_bot.rb
+++ app/models/slack_bot.rb
@@ -3,9 +3,9 @@ class SlackBot < ActiveRecord::Base
 
   def self.from_auth_hash(auth_hash)
     bot_info = auth_hash[:extra][:bot_info]
-    bot = find_by(id: bot_info[:bot_access_token])
+    bot = find_by(id: bot_info.fetch(:bot_access_token))
     if bot
-      bot.update_attributes(:access_token => bot_info[:bot_access_token])
+      bot.update_attributes(access_token: bot_info.fetch(:bot_access_token))
     else
       create(id: bot_info[:bot_access_token]) do |bot|
         bot.access_token = bot_info[:bot_access_token]
